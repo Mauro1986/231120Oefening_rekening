055463f2b47bbac3dab0ee02538d6eb7f740b1b2
MAURO GAY <MAURO.GAY@student.intecbrussel.be>
2020-11-23 15:38:18 +0100

    Add project files.

---
+++ 231120Oefening_rekening/Models/Rekening.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _231120Oefening_rekening.Models
+{
+    public class Rekening
+    {
+        private string rekeningnummer;
+
+        public string Rekeningnummer
+        {
+            get { return rekeningnummer; }
+            set
+            {
+                if (value.Length != 12)
+                {
+                    Console.WriteLine("Rekeningnummer is niet geldig");
+                }
+                else
+                {
+                    rekeningnummer = value;
+                }
+            }
+        }
+        public double Saldo { get; set; }
+
+        public DateTime creatieDatum;
+        public DateTime CreatieDatum
+        {
+            get { return creatieDatum; }
+            set 
+            {
+                if (value.Year < 1990)
+                {
+                    Console.WriteLine("Creatie datum niet geldig(<1990)");
+                }
+                else
+                {
+                    creatieDatum = value;
+                }
+            }
+        }
+
+        public Rekening(string rekeningnummer, double saldo, DateTime creatieDatum)
+        {
+            Rekeningnummer = rekeningnummer;
+            Saldo = saldo;
+            CreatieDatum = creatieDatum;
+        }
+
+        public void Afbeelden()
+        {
+            Console.WriteLine($"Rekeningnummer {Rekeningnummer}");
+            Console.WriteLine($"Saldo {Saldo}");
+            Console.WriteLine($"CreatieDatum {CreatieDatum}");
+        }
+
+        public void Storten(double bedrag)
+        {
+            
+            Saldo = Saldo + bedrag;
+
+        }
+
+        public override string ToString()
+        {
+            return $"Rekeningnummer:{Rekeningnummer}  Saldo:{Saldo}  Creatiedatum:{CreatieDatum.ToShortDateString()}";
+        }
+    }   
+}
---
+++ 231120Oefening_rekening/Program.cs
@@ -0,0 +1,28 @@
+﻿using _231120Oefening_rekening.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace _231120Oefening_rekening
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double input;
+
+            Rekening rekening1 = new Rekening("BE3456789123", 1000, new DateTime(1999,06,01));
+
+            Console.WriteLine(rekening1);
+            Console.WriteLine();
+
+            Console.WriteLine("Hoeveel wilt u storten?");
+            input = Convert.ToDouble(Console.ReadLine());
+            rekening1.Storten(input);
+            Console.WriteLine(rekening1);
+        }
+    }
+}
